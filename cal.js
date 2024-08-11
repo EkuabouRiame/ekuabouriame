c1d610f8a9fd142874753fef4ddfdf386aa18b36
EkuabouRiame <wikhuang@gmail.com>
2024-08-12 00:15:47 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,25 @@
+// Calculator
+
+const display = document.getElementById('display');
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+}
+function calculate(){
+    try{
+    display.value = eval(display.value);
+    }
+    catch{
+    display.value = "Error";
+    }
+}
+function del(){
+    display.value = display.value.toString().slice(0,-1);
+}
